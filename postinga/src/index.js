b41cf93ded31bd3822d9b6e941eb998351705c98
ntwari-egide <ntwariegide2@gmail.com>
2020-12-04 07:26:38 +0200

    Starting rendering react into my redux

--- postinga/src/index.js
+++ postinga/src/index.js
@@ -1,38 +1,39 @@
-import axios from "axios"
-import postsStore from "./store/postsStore";
-import * as postActions from "./actions/postActions"
+// import axios from "axios"
+// import postsStore from "./store/postsStore";
+// import * as postActions from "./actions/postActions"
+//
+// postsStore.subscribe(()=> {
+//     console.log("state changed !",postsStore.getState())
+// })
+//
+//
+//
+//
+// let post = {
+//     userId: 1,
+//     id: 5,
+//     title: "Created --- nesciunt quas odio",
+//     body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+// }
+//
+//
+// let updatePost = {
+//     userId: 2,
+//     id: 1,
+//     title: "--- updated ---- nesciunt quas odio ",
+//     body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+// }
+//
+// // console.log(typeof ourData[0])
+// postActions.resetPostStore()
+//
+// postActions.getAllPosts()
+//
+// postActions.getPostById(1)
+//
+// postActions.createPost(post)
+//
+// postActions.updatePost(updatePost)
+//
+// postActions.deletePost(3)
 
-postsStore.subscribe(()=> {
-    console.log("state changed !",postsStore.getState())
-})
-
-
-
-
-let post = {
-    userId: 1,
-    id: 5,
-    title: "Created --- nesciunt quas odio",
-    body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-}
-
-
-let updatePost = {
-    userId: 2,
-    id: 1,
-    title: "--- updated ---- nesciunt quas odio ",
-    body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-}
-
-// console.log(typeof ourData[0])
-postActions.resetPostStore()
-
-postActions.getAllPosts()
-
-postActions.getPostById(1)
-
-postActions.createPost(post)
-
-postActions.updatePost(updatePost)
-
-postActions.deletePost(3)
