f2752778504a0bc041199b613ff70ac92f4ce139
ntwari-egide <ntwariegide2@gmail.com>
2020-12-04 13:00:23 +0200

    Added the html content of Create Post component

--- postinga/src/components/postComponents/CreatePostComponent.js
+++ postinga/src/components/postComponents/CreatePostComponent.js
@@ -1,9 +1,24 @@
-import React from "react"
+import React,{useContext} from "react"
+
+const Context = React.createContext
 
 export default function CreatePostComponent(){
     return (
-        <div>
-            <h1>Added compoent</h1>
-        </div>
+            <div>
+                <div className="card">
+                    <h2 className="align-center  font-weight-bold">Create Post</h2><br/><br/>
+                    <form>
+                        <div className="form-group">
+                           <input type="text"/><br/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlInput1">Post Content</label>
+                            <textarea type="text" name="testimonialBody" placeholder="Enter testimony body" rows="8" ></textarea>
+                        </div>
+                    </form>
+
+                    <input type="submit" value="Post Testimony"/>
+                </div>
+            </div>
     )
 }
