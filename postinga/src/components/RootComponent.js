95de76e6a4a0f7a4872ef387b5b06805e1bae10c
ntwari-egide <ntwariegide2@gmail.com>
2020-12-04 09:46:51 +0200

    Added welcome post component

--- postinga/src/components/RootComponent.js
+++ postinga/src/components/RootComponent.js
@@ -1,10 +1,13 @@
 import React from "react"
 import NavbarComponent from "./templates/NavbarComponent";
+import FooterComponent from "./templates/FooterComponent";
 
 export default function RootComponent(){
     return (
         <div>
             <NavbarComponent />
+
+            <FooterComponent />
         </div>
     )
 }
