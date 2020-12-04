de7126b85b53aab05e22d990c3f02e26d2156479
ntwari-egide <ntwariegide2@gmail.com>
2020-12-04 12:48:03 +0200

    Route create post component

--- postinga/src/components/RootComponent.js
+++ postinga/src/components/RootComponent.js
@@ -3,14 +3,15 @@ import NavbarComponent from "./templates/NavbarComponent";
 import FooterComponent from "./templates/FooterComponent";
 import WelcomeComponent from "./postComponents/WelcomeComponent";
 import {BrowserRouter as Router,Route} from "react-router-dom";
+import CreatePostComponent from "./postComponents/CreatePostComponent";
 export default function RootComponent(){
     return (
         <Router>
             <div>
                 <NavbarComponent />
                 <Route exact path="/" component={WelcomeComponent} />
-                <Route exact path="/create-post"/>
-                <Route exact path="/edit-post"/>
+                <Route exact path="/create-post" component={CreatePostComponent} />
+                <Route exact path="/edit-post" />
                 <FooterComponent />
             </div>
         </Router>
