bab64cedde66edb8f4f22d1280060619a04dda1c
ntwari-egide <ntwariegide2@gmail.com>
2020-12-04 13:05:49 +0200

    Set the state for the create post component

--- postinga/src/components/postComponents/CreatePostComponent.js
+++ postinga/src/components/postComponents/CreatePostComponent.js
@@ -1,19 +1,28 @@
-import React,{useContext} from "react"
+import React, {useContext, useState} from "react"
+import {useDispatch} from "react-redux";
 
 const Context = React.createContext
 
 export default function CreatePostComponent(){
+
+    const dispatch = useDispatch()
+    const [postState,createPost] = useState({
+        userId: 2,
+        title: "FIRST nesciunt quas odio ",
+        body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    })
+
     return (
             <div>
                 <div className="card">
                     <h2 className="align-center  font-weight-bold">Create Post</h2><br/><br/>
                     <form>
                         <div className="form-group">
-                           <input type="text"/><br/>
+                           <input type="text" name="title" value={postState.title}/><br/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleFormControlInput1">Post Content</label>
-                            <textarea type="text" name="testimonialBody" placeholder="Enter testimony body" rows="8" ></textarea>
+                            <textarea name="body" value={postState.body} type="text" name="testimonialBody" placeholder="Enter testimony body" rows="8" ></textarea>
                         </div>
                     </form>
 
