8b382527ce18140d0f7718cb4954196256fd649c
ntwari-egide <ntwariegide2@gmail.com>
2020-12-04 11:58:07 +0200

    Getting all posts is working

--- postinga/src/reducers/PostsReducer.js
+++ postinga/src/reducers/PostsReducer.js
@@ -10,7 +10,7 @@ let initialPostState = [
     {
         userId: 2,
         id: 2,
-        title: "FIRST nesciunt quas odio ",
+        title: "SECOND nesciunt quas odio ",
         body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
     }
 ];
