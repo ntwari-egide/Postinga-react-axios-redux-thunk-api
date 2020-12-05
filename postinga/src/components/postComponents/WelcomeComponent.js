3728f8db7da76e06e42ceb31f14579523365e1bb
ntwari-egide <ntwariegide2@gmail.com>
2020-12-05 08:58:04 +0200

    Created comment component in different components

--- postinga/src/components/postComponents/WelcomeComponent.js
+++ postinga/src/components/postComponents/WelcomeComponent.js
@@ -6,6 +6,7 @@ import * as postActions from "./../../actions/postActions"
 import * as commentActions from "../../actions/CommentActions"
 import {Link} from "react-router-dom";
 import commentStore from "../../store/CommentStore";
+import CommentComponent from "./CommentComponent";
 
 const Content = React.createContext()
 
@@ -54,7 +55,7 @@ function CurrentPost({...postContent}){
                 <h6>Title : {postContent.title}</h6>
                 <p>Message  : {postContent.body}</p>
 
-                <Get_Comment_by_Id postId={postContent.id} />
+                <CommentComponent postId={postContent.id} />
 
                 <Link type="button" class="btn btn-primary" to="/edit" to={{pathname: "/edit-post",state: postContent}}>Edit</Link>
 
@@ -70,49 +71,5 @@ function CurrentPost({...postContent}){
             </div>
 
 
-    )
-}
-
-function Get_Comment_by_Id({postId}){
-    // commentStore.subscribe(()=>{
-    //     console.log("Comment changed : ",commentStore.getState())
-    // })
-    useEffect(()=>{
-        commentActions.get_comment_by_oid(postId)
-    },[])
-    
-
-    const commentState =() => {
-        return commentStore.getState()
-    }
-
-    const allComments = useSelector(commentState)
-
-    return (
-        <div>
-            <h6 className="text-center">Comments : </h6><br />
-            <div className="row">
-                <ListOfComments allComments={allComments} />
-            </div>
-        </div>
-    )
-}
-
-function ListOfComments({allComments}){
-    return allComments.map(comment =>
-        <CommentContent key={comment.id} {...comment} />
-    )
-}
-
-function CommentContent({...comment}){
-    console.log(comment)
-    return (
-        <div>
-            <div className="col-sm-8">
-                <p className="text-primary">Title : {comment.Name}</p>
-                <p>Message  : {comment.Body}</p>
-                <p className="text-success">commented by : {comment.Email}</p>
-            </div>
-        </div>
     )
 }
