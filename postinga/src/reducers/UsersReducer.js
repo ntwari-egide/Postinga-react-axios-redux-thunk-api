ab0b6d3898f13f8e2b6ac029fd14edc02f28db53
ntwari-egide <ntwariegide2@gmail.com>
2020-12-05 09:48:41 +0200

    Completed user reducer

--- postinga/src/reducers/UsersReducer.js
+++ postinga/src/reducers/UsersReducer.js
@@ -8,5 +8,23 @@ function UsersReducer(state = initialState,action){
 
         case actionTypes.GET_USER:
             return state.filter(user => user.id === action.payload.id)
+
+        case actionTypes.ADD_USER:
+            return [
+                ...state,action.paylaod
+            ]
+
+        case actionTypes.UPDATE_USER:
+            return state.map(user => {
+                if(user.id === action.payload.id){
+                    return action.payload
+                }
+            })
+
+        case actionTypes.DELETE_USER:
+            return state.filter(user => user.id !== action.payload.id)
+
+        default:
+            return state
     }
 }
