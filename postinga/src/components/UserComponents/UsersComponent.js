ba40578bfdef9ed744552c1f8ac7b9fd9da8291d
ntwari-egide <ntwariegide2@gmail.com>
2020-12-05 10:14:59 +0200

    Rendering all users from the api

--- postinga/src/components/UserComponents/UsersComponent.js
+++ postinga/src/components/UserComponents/UsersComponent.js
@@ -1,5 +1,15 @@
-import React from "react"
-
+import React,{useEffect} from "react"
+import * as userActions from "../../actions/UsersActions/UserActions"
+import {get_all_users} from "../../actions/UsersActions/UserActions";
 export default function UsersComponent(){
 
+    useEffect(()=>{
+        userActions.get_all_users()
+    },[])
+
+    return (
+        <div>
+
+        </div>
+    )
 }
