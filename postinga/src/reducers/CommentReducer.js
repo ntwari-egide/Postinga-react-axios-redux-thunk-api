cf745242cce246f07dee722dfc08ac884d6c2403
ntwari-egide <ntwariegide2@gmail.com>
2020-12-05 07:02:29 +0200

    Enabled the comment function in reducer

--- postinga/src/reducers/CommentReducer.js
+++ postinga/src/reducers/CommentReducer.js
@@ -1,6 +1,9 @@
 let initialState = []
 export default function CommentReducer(state= initialState,action){
     switch (action.type) {
-
+        case "set_comment":
+            return action.payload
+        default:
+            return state
     }
 }
