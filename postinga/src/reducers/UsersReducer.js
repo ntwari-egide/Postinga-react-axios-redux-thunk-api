d6a3363f45527b9f1768d505070ba19d8ad5dc6e
ntwari-egide <ntwariegide2@gmail.com>
2020-12-05 09:44:59 +0200

    Applying the users action into reducer

--- postinga/src/reducers/UsersReducer.js
+++ postinga/src/reducers/UsersReducer.js
@@ -1,7 +1,12 @@
+import * as actionTypes from "../actions/UsersActions/UsersActionTypes"
 const initialState = []
 
 function UsersReducer(state = initialState,action){
     switch (action.type) {
+        case actionTypes.GET_ALL_USERS:
+            return action.paylaod
 
+        case actionTypes.GET_USER:
+            return state.filter(user => user.id === action.payload.id)
     }
 }
