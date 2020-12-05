a1f80a865280629593c4483bf51158d4d2b145ef
ntwari-egide <ntwariegide2@gmail.com>
2020-12-05 10:10:35 +0200

    Routing users component

--- postinga/src/components/RootComponent.js
+++ postinga/src/components/RootComponent.js
@@ -5,6 +5,7 @@ import WelcomeComponent from "./postComponents/WelcomeComponent";
 import {BrowserRouter as Router,Route} from "react-router-dom";
 import CreatePostComponent from "./postComponents/CreatePostComponent";
 import EditPostComponent from "./postComponents/EditPostComponent";
+import UsersComponent from "./UserComponents/UsersComponent";
 export default function RootComponent(){
     return (
         <Router>
@@ -12,7 +13,8 @@ export default function RootComponent(){
                 <NavbarComponent />
                 <Route exact path="/" component={WelcomeComponent} />
                 <Route exact path="/create-post" component={CreatePostComponent} />
-                <Route exact path="/edit-post" component={EditPostComponent}/>
+                <Route path="/edit-post" component={EditPostComponent}/>
+                <Route path="/users" component={UsersComponent} />
                 <FooterComponent />
             </div>
         </Router>
