3801ac5a313d0d748504b408fec441c0ea04b40e
ntwari-egide <ntwariegide2@gmail.com>
2020-12-05 09:55:43 +0200

    Finished working on the users actions

--- postinga/src/actions/UsersActions/UserActions.js
+++ postinga/src/actions/UsersActions/UserActions.js
@@ -1,6 +1,15 @@
 import userStore from "../../store/UsersStore";
+import * as actions from "./UsersActionTypes"
 import axios from  "axios"
 
 const get_all_users = () => {
     const allUsersHandler =  axios.get(`https://jsonplaceholder.typicode.com/users`)
+    userStore.dispatch(getAllUsersAction(allUsersHandler.data))
+}
+
+const getAllUsersAction = (allUsers) => {
+    return {
+        type: actions.GET_ALL_USERS,
+        payload: allUsers
+    }
 }
