d957fb5c8e35d7c17ba2a4ec0abccc2c2d72378d
ntwari-egide <ntwariegide2@gmail.com>
2020-12-06 18:33:22 +0200

    Working on changing the usage of store

--- postinga/src/reducers/PostsReducer.js
+++ postinga/src/reducers/PostsReducer.js
@@ -1,6 +1,13 @@
 import * as actions from "../actions/postActionTypes"
 
-let initialPostState = [];
+let initialPostState = [
+    {
+        userId: 1,
+        id: 1,
+        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+];
 
 export default function postReducer(state = initialPostState,action){
     switch (action.type){
