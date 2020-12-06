4c250fed78205909bc397261c14dcfb67619fe5b
ntwari-egide <ntwariegide2@gmail.com>
2020-12-06 14:30:06 +0200

    Imported all necessary stores

---
+++ postinga/src/store/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "redux";
+import postsStore from "./postsStore";
+import userStore from "./UsersStore";
+import commentStore from "./CommentStore";
+
+export default combineReducers({
+
+})
